978903f7e94454d421a0523ff83e0e0a7e006caf
Kavish Vijan <kavishvijan461@gmail.com>
2025-05-28 02:03:03 +0530

    Add files via upload
    Task 1: Perform Basic Mathematical Operations

---
+++ task1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+
+p=input("enter first number:")
+n=input("enter second number:")
+p=int(p)
+n=int(n)
+
+print(p+n)
+print(p-n)
+print(n*p)
+print(p/n)
+
