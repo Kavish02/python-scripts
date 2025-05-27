e809b55646a7ca3ea2bf5529cb7130af44035ba0
Kavish Vijan <kavishvijan461@gmail.com>
2025-05-28 02:15:45 +0530

    Add files via upload
    Task 2: Create a Personalized Greeting

---
+++ task2.py
@@ -0,0 +1,7 @@
+#Task 2: Create a Personalized Greeting
+
+a=(input("enter your first name:"))
+b=(input("enter your last name:"))
+
+c= a+" "+b
+print(f"hello,jay {c}! welcome to python programing")
