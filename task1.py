bc8e4411842a1d9cd0e732dac8ad447ba09b8b7d
Kavish Vijan <kavishvijan461@gmail.com>
2025-05-28 18:44:57 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+#Task 1: Perform Basic Mathematical Operations
+from stringprep import in_table_c5
+
+p=input("enter first number:")
+n=input("enter second number:")
+p=int(p)
+n=int(n)
+print("addition:",p+n)
+print("substraction:",p-n)
+print("multiplication:",n*p)
+print("division:",p/n)
+
